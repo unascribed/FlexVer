49e092fa1fc9a8376eaf370ede1e01bc2baaa5ae
Una Thompson <me@unascribed.com>
2023-01-16 17:17:56 -0800

    js: Fix build not working with no dist dir, fix workflow path

--- js/build.js
+++ js/build.js
@@ -3,6 +3,7 @@ const handlebars = require("handlebars");
 
 const src = fs.readFileSync('index.js');
 
+fs.mkdirSync('dist');
 ['browser', 'node', 'module'].forEach((e) => {
 	fs.writeFileSync('dist/'+e+'.js', handlebars.compile(fs.readFileSync('templates/'+e+'.js.hbs').toString('utf8'))({src}));
 });

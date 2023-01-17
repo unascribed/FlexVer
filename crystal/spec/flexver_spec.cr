092b976b1a05f60c76304cc348187444d2d9774e
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-01-17 14:18:53 +0300

    crystal: implement #10

--- crystal/spec/flexver_spec.cr
+++ crystal/spec/flexver_spec.cr
@@ -1,31 +1,24 @@
 require "spec"
 require "../src/flexver.cr"
 
+private def parse_test(path : Path) : Array(Tuple(String, Char, String))
+  File.read(path)
+    .split("\n")
+    .reject(&.starts_with?("#"))
+    .reject(&.blank?)
+    .map do |line|
+      words = line.split(" ")
+      {words[0], words[1][0], words[2]}
+    end
+end
+
+TEST_DIR = Path.new("../test")
+TESTS = ["test_vectors.txt", "large.txt"]
+
 describe FlexVer do
-  [
-    {"b1.7.3", '>', "a1.2.6"},
-    {"a1.1.2", '<', "a1.1.2_01"},
-    {"1.16.5-0.00.5", '>', "1.14.2-1.3.7"},
-    {"1.0.0", '<', "1.0.0_01"},
-    {"1.0.1", '>', "1.0.0_01"},
-    {"0.17.1-beta.1", '<', "0.17.1"},
-    {"0.17.1-beta.1", '<', "0.17.1-beta.2"},
-    {"1.4.5_01", '=', "1.4.5_01+exp-1.17"},
-    {"1.4.5_01", '=', "1.4.5_01+exp-1.17-moretext"},
-    {"14w16a", '<', "18w40b"},
-    {"18w40a", '<', "18w40b"},
-    {"0.6.0-1.18.x", '<', "0.9.beta-1.18.x"},
-    {"1.0", '<', "1.1"},
-    {"1.0", '<', "1.0.1"},
-    {"10", '>', "2"},
+  tests = TESTS.map { |file| parse_test(TEST_DIR / file) } .sum
 
-    # codepoint-wise numeric comparasion check
-    {"36893488147419103232", '<', "36893488147419103233"},
-
-    # nonsense comparasions
-    {"1.4.5_01+exp-1.17", '<', "18w40b"},
-    {"13w02a", '<', "c0.3.0_01"},
-  ].each do |a, cmp, b|
+  tests.each do |a, cmp, b|
     it "performs sample comparasions correctly: #{a} #{cmp} #{b}" do
       cmp_int = case cmp
                 when '>'
